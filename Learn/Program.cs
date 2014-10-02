e87aaacc8bab71699e049ff4b4682cbdc71582a2
Learning <carlhen@users.noreply.github.com>
2014-10-02 13:09:41 +0200

    noe gikk galt

--- Learn/Program.cs
+++ Learn/Program.cs
@@ -10,10 +10,29 @@ namespace Learn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("write");
-            int age = int.Parse(Console.ReadLine());
+            int age = 0;
+            int loops = 3;
+            Program p = new Program();
+            Console.WriteLine("Type a number without decimals");
+            string ageIn = Console.ReadLine();
+            age = p.NumberWithoutDecimals(ageIn, loops, age);
             Console.WriteLine(age);
             Console.ReadLine();
+        }
+        int NumberWithoutDecimals(string agetemp,int atempt, int age)
+        {
+            for (int i = 0; i < atempt; i++)
+			{
+			    bool ageCheck = false;
+                ageCheck = int.TryParse(agetemp, out age);
+                if (ageCheck == true)
+                {
+                    return age;
+                    break; 
+                }
+                else
+                    Console.WriteLine("Try again, and remmember: Do not type a number WITH decimals"); 
+			}
         }
     }
 }
