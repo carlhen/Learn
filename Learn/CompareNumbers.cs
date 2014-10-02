4b02eee33680fb33eadd2c98233cdfe2f27a4bd6
Learning <carlhen@users.noreply.github.com>
2014-10-02 14:35:57 +0200

    upload

---
+++ Learn/CompareNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn
+{
+    class CompareNumbers
+    {
+        {
+        static void Main(string[] args)
+        {
+            Program p = new Program();//constructor; makes a new object of type Program
+            
+            //CH, how to call the method below? You write some code!
+        }
+
+        /// <summary>
+        /// Compares two numbers
+        /// </summary>
+        /// <param name="num1">Number to be compared</param>
+        /// <param name="num2">Number to be compared</param>
+        /// <returns>The max of two numbers</returns>
+        int FindMax(int num1, int num2)
+        {
+            /* local variable declaration */
+            int result;
+
+            if (num1 > num2)
+                result = num1;
+            else
+                result = num2;
+
+            return result;
+        }
+    }
+}
